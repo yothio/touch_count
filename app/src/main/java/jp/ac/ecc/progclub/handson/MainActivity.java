0451654806b9ba31dc03dafbcc65bf9b31c46976
shiota yoshiki <shiotyan317@gmail.com>
2018-05-07 19:17:40 +0900

    add progress bar, countdown text

--- app/src/main/java/jp/ac/ecc/progclub/handson/MainActivity.java
+++ app/src/main/java/jp/ac/ecc/progclub/handson/MainActivity.java
@@ -1,7 +1,10 @@
 package jp.ac.ecc.progclub.handson;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CountDownActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
