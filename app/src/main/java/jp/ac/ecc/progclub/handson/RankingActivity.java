2dfd68babaf60a78d04c9529d61a73f04bb60353
shiota yoshiki <shiotyan317@gmail.com>
2018-05-14 03:55:35 +0900

    [add] test date

--- app/src/main/java/jp/ac/ecc/progclub/handson/RankingActivity.java
+++ app/src/main/java/jp/ac/ecc/progclub/handson/RankingActivity.java
@@ -4,12 +4,15 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 public class RankingActivity extends AppCompatActivity {
@@ -19,7 +22,6 @@ public class RankingActivity extends AppCompatActivity {
     ImageView rankImage;
     SharedPreferences sp;
 
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,10 +30,19 @@ public class RankingActivity extends AppCompatActivity {
         rankButton = findViewById(R.id.rank_button);
         rankinglist = findViewById(R.id.rank_listview);
         rankImage = findViewById(R.id.rank_imageview);
+        List<User> list = new ArrayList<>();
+        list.add(new User("test",111));
+        list.add(new User("test1",111));
+        list.add(new User("test2",111));
+
+
+        ArrayAdapter<User> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        rankinglist.setAdapter(adapter);
+
     }
 
 
-    private ArrayList getRanking() {
+    private ArrayList<User> getRanking() {
 
         ArrayList<User> result = new ArrayList<User>();
         sp = getSharedPreferences("Ranking", MODE_PRIVATE);
