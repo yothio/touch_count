6e4c77d1f71e812c45348a5ec3648d147f585033
shiota yoshiki <shiotyan317@gmail.com>
2018-05-14 02:00:41 +0900

    [add] activity can not tap the back key

--- app/src/main/java/jp/ac/ecc/progclub/handson/BaseActivity.java
+++ app/src/main/java/jp/ac/ecc/progclub/handson/BaseActivity.java
@@ -1,8 +1,24 @@
 package jp.ac.ecc.progclub.handson;
 
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+
 /**
  * Created by yocchi on 2018/05/14.
  */
 
-public class BaseActivity {
+public abstract class BaseActivity extends AppCompatActivity {
+//    今回のアプリは戻るボタンを反応しないように設定
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if (event.getAction()==KeyEvent.ACTION_DOWN) {
+            switch (event.getKeyCode()) {
+                case KeyEvent.KEYCODE_BACK:
+                    // ダイアログ表示など特定の処理を行いたい場合はここに記述
+                    // 親クラスのdispatchKeyEvent()を呼び出さずにtrueを返す
+                    return true;
+            }
+        }
+        return super.dispatchKeyEvent(event);
+    }
 }
