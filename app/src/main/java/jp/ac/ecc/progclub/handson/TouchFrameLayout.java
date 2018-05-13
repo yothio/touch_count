d7e9555029cb8218ec9e1d7854ad4b2129619da4
shiota yoshiki <shiotyan317@gmail.com>
2018-05-14 02:02:47 +0900

    [add] touch event enable custom layout

--- app/src/main/java/jp/ac/ecc/progclub/handson/TouchFrameLayout.java
+++ app/src/main/java/jp/ac/ecc/progclub/handson/TouchFrameLayout.java
@@ -1,8 +1,36 @@
 package jp.ac.ecc.progclub.handson;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
 /**
  * Created by yocchi on 2018/05/14.
  */
 
-public class TouchFrameLayout {
+public class TouchFrameLayout extends FrameLayout {
+    public TouchFrameLayout(@NonNull Context context) {
+        super(context);
+    }
+
+    public TouchFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TouchFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public TouchFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    // frameLayoutにタッチイベントを付ける際に警告を消すために下記メソッドをオーバーライド
+    @Override
+    public boolean performClick() {
+        super.performClick();
+        return true;
+    }
 }
