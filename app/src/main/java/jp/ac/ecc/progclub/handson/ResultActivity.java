194683e0d542406605c848bed19e79fb9eb10d7a
shiota yoshiki <shiotyan317@gmail.com>
2018-05-14 03:16:16 +0900

    [add] get tap count

--- app/src/main/java/jp/ac/ecc/progclub/handson/ResultActivity.java
+++ app/src/main/java/jp/ac/ecc/progclub/handson/ResultActivity.java
@@ -8,33 +8,45 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import static jp.ac.ecc.progclub.handson.countdown.CountDownActivity.COUNT_KEY;
+
 public class ResultActivity extends AppCompatActivity {
 
     static final String save_resultName = "result";                   // 保存オブジェクト名
     static final String save_resultKey_name = "resultName";          // 入力した名前の保存キー名
     static final String save_resultKey_clickNum = "resultClickNum"; // 　クリック数の保存キー名
 
+    TextView nameText;
+    TextView countText;
+    Button nextBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        final TextView nameTV = findViewById(R.id.NameEditText);    // 名前入力用テキストボックス
-        final TextView countTV = findViewById(R.id.NumTextView);    // カウント回数を表示したラベル
-        final Button button = findViewById(R.id.DecButton);         // [決定]ボタン
+        nameText = findViewById(R.id.NameEditText);    // 名前入力用テキストボックス
+        countText = findViewById(R.id.NumTextView);    // カウント回数を表示したラベル
+        nextBtn = findViewById(R.id.DecButton);        // [決定]ボタン
 
-        button.setOnClickListener(new View.OnClickListener() {
+        // 前画面でクリックした数を取得
+        final int count = getIntent().getIntExtra(COUNT_KEY,0);
+
+        // カウントテキストにセット
+        countText.setText(String.valueOf(count));
+
+        // データ保存してランキング画面へ
+        nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int clickNum = Integer.parseInt(countTV.getText().toString());  // クリック数
-                String name = nameTV.getText().toString();   // 名前
+                String name = countText.getText().toString();   // 名前
 
                 // クリック数と名前を保存
                 SharedPreferences sharedPreferences =
                         getSharedPreferences(save_resultName, Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.putString(save_resultKey_name, name);
-                editor.putInt(save_resultKey_clickNum, clickNum);
+                editor.putInt(save_resultKey_clickNum, count);
                 editor.apply();
             }
         });
